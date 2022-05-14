4ada36384529c5f5570fa03b9d9657648f10e444
Modesto Amador <mofesdo.dev@gmail.com>
2022-05-14 14:01:48 -0700

    added background images

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-alert("Yo its working");
+//alert("Yo its working");
 var numberOfButtons = document.querySelectorAll(".drum").length;
-
+var audio = new Audio('sounds/tom-1.mp3');
 for(var i = 0; i < numberOfButtons; i++){
 
 document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-
+  var buttonInnerHTML = this.innerHTML;
 }
 
 function handleClick(){
-  alert("I got clicked!");
+  audio.play();
 }
